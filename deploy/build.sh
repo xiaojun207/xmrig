6d734d68c108f633e5cb5187b0733110c59d05d7
name <email>
2019-11-16 10:46:11 +0800

    add build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-sudo yum install -y epel-release
-sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
-
-mkdir build -p && cd build && rm ./* -rf
-cmake .. -DUV_LIBRARY=/usr/lib64/libuv.a
-make
-
-cd ..
-
 app="xmrig"
 DOCKER_BUILD_TAG="latest"
 DOCKER_BASE_REPO="xiaojun207"
@@ -18,7 +9,7 @@ docker build -t ${DOCKER_BASE_REPO}/$app:${DOCKER_BUILD_TAG} -f deploy/Dockerfil
 
 
 # 启动命令
-# docker run --rm -it --name xmrig -e password="ice@post.com" xiaojun207/xmrig:latest
+# docker run --rm -it --name test -e password="ice@post.com" xiaojun207/xmrig:latest
 
 # docker run -d -it --name - xmrig xiaojun207/xmrig:latest
 
